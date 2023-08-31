105c743cf75c4b675baadad46c730277b9088601
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-31 17:43:58 +0900

    style: code formatting

--- src/apis/issues.ts
+++ src/apis/issues.ts
@@ -1,15 +1,14 @@
-import { instance } from './instance';
-import { PATH } from 'constants/api';
-
+import {instance} from './instance';
+import {PATH} from 'constants/api';
 
 export const getIssues = async (page: number, perPage = 24, state = 'open', sort = 'comments') => {
-  const response = await instance.get(
-    `${PATH}?page=${page}&per_page=${perPage}&state=${state}&sort=${sort}`
-  );
-  return response;
+    const response = await instance.get(
+        `${PATH}?page=${page}&per_page=${perPage}&state=${state}&sort=${sort}`
+    );
+    return response;
 };
 
 export const getIssue = async (postId: number) => {
-  const response = await instance.get(`${PATH}/${postId}`);
-  return response;
+    const response = await instance.get(`${PATH}/${postId}`);
+    return response;
 };
