b7e3e4bc2e09758cce8565c0039e40228fab354d
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-31 19:15:46 +0900

    feat: Issue list container 컴포넌트 생성

---
+++ src/containers/IssueListContainer.tsx
@@ -0,0 +1,50 @@
+import {IssuesController} from 'controllers/IssuesController';
+
+import Ad from 'components/Ad';
+import {useRecoilValue} from 'recoil';
+import {issuesStateAtom} from 'stores/atom';
+import {useEffect} from 'react';
+import NotFound from 'pages/NotFound';
+import {useInfiniteScroll} from 'hooks/useInfiniteScroll';
+import Item from 'components/Item';
+import React from 'react';
+import LoadingSkeleton from 'components/common/LoadingSkeleton';
+import LoadingSpinner from 'components/common/LoadingSpinner';
+
+const IssueListContainer = () => {
+    const issuesState = useRecoilValue(issuesStateAtom);
+    const {isLoading, errorStatus, moreData, issues} = issuesState;
+    const isRefetchNeeded = !issues.length;
+
+    const {getIssues, getNextPage} = IssuesController();
+
+    useEffect(() => {
+        isRefetchNeeded && getIssues(1);
+    }, []);
+
+    const getNextPageRef = useInfiniteScroll(getNextPage);
+
+    if (errorStatus) return <NotFound />;
+
+    return (
+        <>
+            {issues.length > 0 ? (
+                issues.map((data, index) => {
+                    const showAd = (index + 1) % 4 === 0;
+                    return (
+                        <React.Fragment key={data.number}>
+                            <Item issue={data} />
+                            {showAd && <Ad />}
+                        </React.Fragment>
+                    );
+                })
+            ) : (
+                <LoadingSkeleton />
+            )}
+            {isLoading && !isRefetchNeeded && <LoadingSpinner />}
+            {moreData && <div ref={getNextPageRef} />}
+        </>
+    );
+};
+
+export default IssueListContainer;
