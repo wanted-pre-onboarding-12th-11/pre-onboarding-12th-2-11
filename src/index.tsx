6bae35046e1c260d4c4c0cbb3b044a7aa40e71ae
youngchae <010101chae@gmail.com>
2023-08-31 21:59:32 +0900

    design: global.css 색상변수 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/reset.css';
+import './styles/global.css';
 import {RouterProvider} from 'react-router-dom';
 import {Router} from 'Router';
 import {RecoilRoot} from 'recoil';
