4678170a06d401b12349f9e34c54b16ac0b5fe93
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-31 19:19:39 +0900

    feat: 파일 진입점에 리코일 세팅 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './styles/reset.css';
 import {RouterProvider} from 'react-router-dom';
 import {Router} from 'Router';
+import {RecoilRoot} from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <React.StrictMode>
+    <RecoilRoot>
         <RouterProvider router={Router} />
-    </React.StrictMode>
+    </RecoilRoot>
 );
