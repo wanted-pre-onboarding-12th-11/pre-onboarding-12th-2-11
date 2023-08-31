229513efb0f7c3a646968f080d84f77b4f73c627
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-31 18:11:11 +0900

    feat: Ad 컴포넌트 생성

---
+++ src/components/Ad.tsx
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Ad = () => {
+    return (
+        <AdStyled>
+            <Link to='https://www.wanted.co.kr/' target='_blank'>
+                <span className='ad-tag'>ad.</span>
+                <img
+                    src='https://cdn.discordapp.com/attachments/1143474691118485558/1146132601518686371/ad_image.png'
+                    alt='원티드 광고 이미지'
+                ></img>
+            </Link>
+        </AdStyled>
+    );
+};
+
+export default Ad;
+
+const AdStyled = styled.div``;
