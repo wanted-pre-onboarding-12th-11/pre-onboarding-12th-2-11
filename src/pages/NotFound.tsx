c1f19a7c2d822f21ed01e3510aa085c862720cd0
youngchae <010101chae@gmail.com>
2023-08-31 21:59:56 +0900

    feat: 에러페이지 추가

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,51 @@
-const NotFound = () => {
-    return <></>;
+import ROUTES from 'constants/routes';
+import {useNavigate} from 'react-router-dom';
+import {styled} from 'styled-components';
+
+const NotFound = ({errorStatus}: {errorStatus?: number | string}) => {
+    const navigate = useNavigate();
+
+    const navigateToMain = () => {
+        navigate(ROUTES.ISSUES);
+    };
+
+    return (
+        <StyledNotFoundContainer>
+            <p className='error-status'>{errorStatus && errorStatus}</p>
+            <p className='message'>페이지를 찾을 수 없습니다 :(</p>
+            <button onClick={navigateToMain}>메인으로 돌아가기</button>
+        </StyledNotFoundContainer>
+    );
 };
 
 export default NotFound;
+
+const StyledNotFoundContainer = styled.div`
+    margin-top: 50px;
+    text-align: center;
+    .error-status {
+        margin: 0;
+        font-size: 70px;
+        font-weight: 900;
+        color: var(--highLighting);
+    }
+
+    .message {
+        padding-top: 0px;
+        margin: 20px 0;
+        font-size: 26px;
+        font-weight: 800;
+        color: var(--textSubtitle);
+    }
+
+    button {
+        padding: 16px;
+        font-size: 18px;
+        font-weight: 600;
+        color: white;
+        background-color: var(--highLighting);
+        border: none;
+        border-radius: 12px;
+        cursor: pointer;
+    }
+`;
