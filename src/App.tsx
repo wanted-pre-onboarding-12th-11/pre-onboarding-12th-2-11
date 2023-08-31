e54f38c3d0583dd136298aaf6f69794aba0ef9c4
youngchae <010101chae@gmail.com>
2023-08-31 21:47:39 +0900

    design: 헤더 제외 컨텐츠 max-width 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import {Outlet} from 'react-router-dom';
 import Header from 'components/common/Header';
+import {styled} from 'styled-components';
 
 const App = () => {
     return (
         <>
             <Header />
-            <Outlet />
+            <ContentsWrapper>
+                <Outlet />
+            </ContentsWrapper>
         </>
     );
 };
 
 export default App;
+
+const ContentsWrapper = styled.div`
+    max-width: 768px;
+    margin: 0 auto;
+`;
