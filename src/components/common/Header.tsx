a26ca8af6e07ee18011c42ac730dd173b7481972
youngchae <010101chae@gmail.com>
2023-08-31 21:46:58 +0900

    feat: 헤더 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,30 @@
+import {OWNER, REPO} from 'constants/api';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
 const Header = () => {
-    return <></>;
+    return (
+        <HeaderContainer>
+            <StyledLink to={'/'}>
+                {OWNER} / {REPO}
+            </StyledLink>
+        </HeaderContainer>
+    );
 };
 
 export default Header;
+
+const HeaderContainer = styled.header`
+    width: 100%;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid lightgray;
+`;
+const StyledLink = styled(Link)`
+    color: black;
+    text-decoration: none;
+    font-size: 30px;
+    font-weight: 700;
+`;
