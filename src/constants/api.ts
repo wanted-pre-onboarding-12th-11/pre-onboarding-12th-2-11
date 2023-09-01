2bc312bedddac0a6972fbdfbda547573bf706a4b
saem <saemilee01@gmail.com>
2023-09-01 14:18:28 +0900

    fix: 다른 레포 이슈 테스트를 위한 상수 변경

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
 export const BASE_URL = 'https://api.github.com';
-export const OWNER = 'facebook';
-export const REPO = 'react';
+export const OWNER = 'saemileee';
+export const REPO = 'wanted-pre-onboarding-12th-2nd';
 export const PATH = `/repos/${OWNER}/${REPO}/issues`;
